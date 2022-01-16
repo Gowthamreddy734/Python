7540cf086a5b01737d257bd261875b486b8dad2f
Gowthamreddy734 <65007561+Gowthamreddy734@users.noreply.github.com>
2022-01-16 12:15:41 +0530

    Add files via upload

---
+++ convert_datatypes.py
@@ -0,0 +1,45 @@
+
+#converting integer to float,bool, string
+number_1 = 10
+number_2 =float(number_1)
+number_3=bool(number_1)
+number_4 =str(number_1)
+print(number_1)
+print(number_2)
+print(number_3)
+print(number_4 )
+
+
+#convert float to string,bool,int
+
+var =10.5
+var1 =int(var)
+var2=bool(var)
+var3 =str(var)
+print(var)
+print(var1)
+print(var2)
+print(var3)
+
+
+#convert boolean to string, int,float 
+
+bool_val= False
+bool_val1=int(bool_val)
+bool_val2=float(bool_val)
+bool_val3 =str(bool_val)
+print(bool_val)
+print(bool_val1)
+print(bool_val2)
+print(bool_val3)
+
+#convert string to int ,flaot ,boolean
+
+string_value = "Python"
+#string_value1 =(type(int(string_value) 
+#string_value2=float(string_value)
+string_value3=bool(string_value)
+print(string_value)
+#print(string_value1)
+#print(string_value2)
+print(string_value3)
